05efe214a761ada8a117d6faa04f7ca0601a4dc3
家承 <57313516+soulkey373@users.noreply.github.com>
2026-06-10 15:45:11 +0000

    update bot

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,13 @@
 // 填入 Firebase 專案設定後，遊戲會自動啟用線上房間同步。
 // 到 Firebase Console 建立 Web App，複製 firebaseConfig 物件貼到下面。
 //
-// window.PEAK_FIREBASE_CONFIG = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_PROJECT.firebaseapp.com",
-//   databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-//   projectId: "YOUR_PROJECT",
-//   storageBucket: "YOUR_PROJECT.appspot.com",
-//   messagingSenderId: "YOUR_SENDER_ID",
-//   appId: "YOUR_APP_ID"
-// };
+window.PEAK_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDw5Yj1_sYZe8vURvetMd7vKVRH1gBF_h8",
+  authDomain: "peak-party-game.firebaseapp.com",
+  databaseURL: "https://peak-party-game-default-rtdb.firebaseio.com",
+  projectId: "peak-party-game",
+  storageBucket: "peak-party-game.firebasestorage.app",
+  messagingSenderId: "18078883290",
+  appId: "1:18078883290:web:2b5efa985d5ca6fb02f4e3"
+};
 
-window.PEAK_FIREBASE_CONFIG = null;
